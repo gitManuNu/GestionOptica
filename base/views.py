7e98f188a1f9814177632f80940c8c215e60c18d
Lucas Vacchino <lnvacchino@gmail.com>
2023-01-06 18:46:14 -0300

    Creación de la primera vista

--- base/views.py
+++ base/views.py
@@ -1,3 +1,14 @@
 from django.shortcuts import render
+from django.http import HttpResponse
+from .models import Persona
 
-# Create your views here.
+def index(request):
+    personas = Persona.objects.all()
+    print(personas)
+    html = render(request, 'personas.html', {
+        'personas':personas
+    })
+    return HttpResponse(html)
+
+
+    
